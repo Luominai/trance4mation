39648975a5a668153685874e2903a2b47c22b34c
Luominai <kevinthepear@gmail.com>
2025-09-05 15:37:44 -0400

    removing some functions that were just there for testing

--- my-phaser-game/src/App.tsx
+++ my-phaser-game/src/App.tsx
@@ -107,135 +107,13 @@ function App()
                 fixedWidth: 80,
                 fixedHeight: 40,
                 wordWrap: {
-                    width: 80
+                    width: 64
                 }
             },
             add: true
         })
         renderTexture.draw(text)
         renderTexture.saveTexture('myTexture')
-    }
-
-    const addPlane = () => {
-        if (!phaserRef.current) {
-            return
-        }
-        if (!phaserRef.current.scene) {
-            return
-        }
-        const scene = phaserRef.current.scene;
-        const x = Phaser.Math.Between(64, scene.scale.width - 64);
-        const y = Phaser.Math.Between(64, scene.scale.height - 64);
-
-        planeRef.current = scene.make.plane({
-            add: true,
-            x: x, y: y,
-            key: 'bg',
-            width: 1, height: 1
-        })
-        planeRef.current.ignoreDirtyCache = true
-
-        const topLeft = [planeRef.current.vertices[0]]
-        const topRight = [planeRef.current.vertices[2], planeRef.current.vertices[5]]
-        const bottomLeft = [planeRef.current.vertices[1], planeRef.current.vertices[3]]
-        const bottomRight = [planeRef.current.vertices[4]]
-
-        const timeline = scene.add.timeline({})
-        const duration = 1000
-        const scale = 1.2
-
-        timeline.add({
-            tween: {
-                targets: [...topLeft, ...topRight, ...bottomLeft, ...bottomRight],
-                duration: duration,
-                x: "*= -1"
-            }
-        })
-        timeline.add({
-            tween: {
-                targets: [...topLeft, ...bottomLeft],
-                duration: duration / 2,
-                y: `*= ${scale}`
-            }
-        })
-        timeline.add({
-            tween: {
-                targets: [...topRight, ...bottomRight],
-                duration: duration / 2,
-                y: `/= ${scale}`
-            }
-        })
-        timeline.add({
-            at: duration / 2,
-            run: () => {
-                planeRef.current?.setTexture('myTexture')
-            }
-        })
-        timeline.add({
-            at: duration / 2,
-            tween: {
-                targets: [...topLeft, ...bottomLeft],
-                duration: duration / 2,
-                y: `/= ${scale}`
-            }
-        })
-        timeline.add({
-            at: duration / 2,
-            tween: {
-                targets: [...topRight, ...bottomRight],
-                duration: duration / 2,
-                y: `*= ${scale}`
-            }
-        })
-
-        timeline.play()
-    }
-
-    const addText = () => {
-        if (!phaserRef.current) {
-            return
-        }
-        if (!phaserRef.current.scene) {
-            return
-        }
-        const scene = phaserRef.current.scene;
-        const x = Phaser.Math.Between(64, scene.scale.width - 64);
-        const y = Phaser.Math.Between(64, scene.scale.height - 64);
-        var rt = scene.make.renderTexture({
-            x: 0,
-            y: 0,
-            width: 32,
-            height: 32,
-        });
-        var txt = scene.make.text({
-            x: x,
-            y: y,
-            padding: {
-                x: 8,    // 32px padding on the left/right
-                y: 8     // 16px padding on the top/bottom
-            },
-            text: 'Text\nGame Object\nCreated from config',
-            style: {
-                fontSize: '12px',
-                fontFamily: 'Arial',
-                color: '#ffffff',
-                align: 'center',  // 'left'|'center'|'right'|'justify'
-                backgroundColor: '#ff00ff',
-                fixedWidth: 100,
-                wordWrap: {
-                    width: 80
-                }
-            },
-            // origin: {x: 0.5, y: 0.5},
-            add: true
-        });
-        console.log(txt)
-        // rt.draw(txt)
-        // rt.saveTexture('myRenderTexture')
-        // console.log(scene.textures)
-
-        // const text = scene.add.sprite(x, y, 'myRenderTexture');
-        // console.log(text)
     }
 
     const addCard = () => {
@@ -262,19 +140,6 @@ function App()
         })
 
         animateCardFlip(scene, card)
-    }
-
-    const addRenderTextureSprite = () => {
-        if (!phaserRef.current) {
-            return
-        }
-        if (!phaserRef.current.scene) {
-            return
-        }
-        const scene = phaserRef.current.scene
-        const x = Phaser.Math.Between(64, scene.scale.width - 64);
-        const y = Phaser.Math.Between(64, scene.scale.height - 64);
-        const text = scene.add.sprite(x, y, 'myTexture');
     }
 
     const animateCardFlip = (scene: Phaser.Scene, card: Phaser.GameObjects.Plane) => {
@@ -358,16 +223,7 @@ function App()
                     <button className="button" onClick={changeScene}>Change Scene</button>
                 </div>
                 <div>
-                    <button className="button" onClick={addPlane}>Add Plane</button>
-                </div>
-                <div>
-                    <button className="button" onClick={addText}>Add Text</button>
-                </div>
-                <div>
                     <button className="button" onClick={addCard}>Add Card</button>
-                </div>
-                <div>
-                    <button className="button" onClick={addRenderTextureSprite}>Add Render Texture Sprite</button>
                 </div>
                 <div>
                     <button className="button" onClick={createTexture}>Create Texture</button>
